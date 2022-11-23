8c1c5d0b0ab4369003722be8419d99e8c4974156
Ramon Barbosa <ramonlegendario21@gmail.com>
2022-11-23 20:02:02 -0300

    oitavo commit

--- src/main/java/com/loja/IndexController.java
+++ src/main/java/com/loja/IndexController.java
@@ -3,14 +3,23 @@ package com.loja;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.loja.model.Categoria;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping("/login")
-	public String index() {
+	public String login() {
 		return "login";
 	}
+	
+	@RequestMapping("/")
+	public String index() {
 
+		return "/index";
+	}
 	
 }
