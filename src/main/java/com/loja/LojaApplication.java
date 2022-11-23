08ae4aae7f32ae489a901540d4221f283bcd0ab6
Ramon Barbosa <ramonlegendario21@gmail.com>
2022-11-23 20:18:15 -0300

    dez commit

--- src/main/java/com/loja/LojaApplication.java
+++ src/main/java/com/loja/LojaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -26,6 +27,13 @@ public class LojaApplication  implements WebMvcConfigurer{
 		SpringApplication.run(LojaApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String index() {
+
+		return "index";
+	}
+	
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login").setViewName("/login");
